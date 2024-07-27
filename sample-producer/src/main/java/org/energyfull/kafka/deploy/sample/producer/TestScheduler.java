916be131105455db73c419156fb80e73a8d33693
LEE KI CHAN <energyfull0216@gmail.com>
2024-07-27 12:13:04 +0900

    fix. test producer 수정
    test application.yaml 작성
    test message 수정

--- sample-producer/src/main/java/org/energyfull/kafka/deploy/sample/producer/TestScheduler.java
+++ sample-producer/src/main/java/org/energyfull/kafka/deploy/sample/producer/TestScheduler.java
@@ -6,16 +6,23 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 @RequiredArgsConstructor
 @Component
 public class TestScheduler {
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private AtomicInteger sequence = new AtomicInteger(0);
+
 
     @Scheduled(fixedRate = 1, timeUnit = TimeUnit.SECONDS)
     public void scheduledTask() {
-        kafkaTemplate.send("test-topic", "test");
+        LocalDateTime now = LocalDateTime.now();
+        int seq = sequence.incrementAndGet();
+        kafkaTemplate.send("test-topic",
+                String.format("message-%d published at %s", seq, now.toString()));
     }
 }
